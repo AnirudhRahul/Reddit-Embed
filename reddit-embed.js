53ee3f659a98f3dbc5a64d3a38db4e66fd279404
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-09-24 20:23:14 -0400

    Added custom styling, cache, and naive div loading

--- reddit-embed.js
+++ reddit-embed.js
@@ -1,12 +1,59 @@
-function embed(url){
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
 
-  xhr.onload = function () {
-    // Request finished. Do processing here.
-    console.log(xhr.responseText)
-  };
+function embed(url, div){
+  cached_json = tryJson(sessionStorage.getItem(url))
 
-  xhr.send();
+  if (cached_json) {
+    process_data(cached_json, div)
+  }
+  else{
+    console.log("Requesting JSON from reddit: " + url)
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = () => {
+      console.log('Response:\n' + xhr.responseText)
+      sessionStorage.setItem(url, xhr.responseText);
+      process_data(JSON.parse(xhr.responseText), div);
+    }
+    xhr.send();
+  }
+}
 
+function process_data(response, div){
+  out = []
+  window.response = response
+  post = response[0].data.children[0].data
+  comments = response[1].data.children
+
+  sr_name = post.subreddit_name_prefixed
+  out.push('<h5>'+sr_name+'</h5>')
+
+  title = post.title
+  // out.push('<h3>'+title+'</h3>')
+
+  link = post.url
+  out.push('<a href="'+ link +'"><h3>'+ title +'</h3></a>')
+
+  console.log(post)
+  console.log(comments)
+
+  skip = 1
+  for(i in comments){
+    const cur = comments[i].data
+    if(i<skip || !cur.body_html)
+      continue
+    const html = he.decode(cur.body_html)
+    console.log(html)
+    out.push(html)
+  }
+
+
+  div.innerHTML = out.join('\n')
+}
+
+function tryJson(str) {
+    try {
+        return JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
 }
