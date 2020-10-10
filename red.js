33d2470e07dacece13be6718aba80f96985668cd
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-10-09 23:23:26 -0400

    Added spoiler link support, and options support

--- red.js
+++ red.js
@@ -37,19 +37,57 @@ var red = function(){
   </div>
   `;
 
-const msPerMinute = 60 * 1000;
+const msPerSecond = 1000;
+const msPerMinute = msPerSecond * 60;
 const msPerHour = msPerMinute * 60;
 const msPerDay = msPerHour * 24;
 const msPerMonth = msPerDay * 30;
 const msPerYear = msPerDay * 365;
 
 const cache_timeout = 30*msPerMinute
+const defaults = {
+  cache_timeout: msPerHour,
+  use_cache: true,
+  show_post_title: true,
+  //TODO create better post support
+  show_post: true,
+  show_comments: true,
+  ignore_bot_comments: true,
+  ignore_sticky_comments: false,
+  depth_limit: -1,
+  comment_limit: [],
+  open_links_in_new_tab: true,
+  recent_post_threshold: 15*msPerMinute,
+  depth_padding: 24,
+  initial_padding: 8,
+  support_spoiler_links: true,
+}
+const spoiler_links = ['/s', '#s', '/spoiler', '#spoiler']
 
-function embed(url, div, useCache = true){
-  cached_json = tryJson(sessionStorage.getItem(url))
 
-  if (useCache && cached_json && Date.now() < cached_json.expire_time) {
-    process_data(JSON.parse(cached_json.response), div)
+function setDefaults(newDefaults) {
+  for(key in newDefaults){
+    if(key in newDefaults){
+      defaults[key] = newDefaults[key]
+    }
+    else{
+      throw key + " is not a valid default option"
+    }
+  }
+}
+
+function add_missing_defaults(opts){
+  // Fill in missings defaults from opts
+  for(key in defaults)
+    if(!(key in opts))
+      opts[key] = defaults[key]
+}
+
+function embed(url, div, opts = defaults){
+  add_missing_defaults(opts)
+  const cached_json = tryJson(sessionStorage.getItem(url))
+  if (opts.use_cache && cached_json && Date.now() < cached_json.expire_time) {
+    renderDiv(JSON.parse(cached_json.response), div, opts)
     console.log("Used Cache")
   }
   else{
@@ -57,89 +95,15 @@ function embed(url, div, useCache = true){
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.onload = () => {
-      sessionStorage.setItem(url, JSON.stringify({
-        response: xhr.responseText,
-        expire_time: Date.now() + cache_timeout
-      }));
-      process_data(JSON.parse(xhr.responseText), div);
+      if(opts.use_cache)
+        sessionStorage.setItem(url, JSON.stringify({
+          response: xhr.responseText,
+          expire_time: Date.now() + opts.cache_timeout
+        }));
+      renderDiv(JSON.parse(xhr.responseText), div, opts);
     }
     xhr.send();
   }
-}
-
-function process_data(response, div){
-  out = []
-  window.response = response
-  post = response[0].data.children[0].data
-  comments = response[1].data.children
-
-  // sr_name = post.subreddit_name_prefixed
-  // out.push('<h3>'+sr_name+'</h5>')
-
-  out.push(Title({
-    title: post.title,
-    post_link: 'https://reddit.com' + post.permalink
-  }))
-
-  comment_q = []
-  for(let i=comments.length-1; i>=0; i--){
-    // Push comment into rendering queue
-    // if it has a body
-    if(comments[i].data.body_html)
-      comment_q.push({
-        cur: comments[i].data,
-        depth: 0
-      })
-  }
-
-  const now = Date.now()
-  initial_shift = 8
-  shift_diff = 24
-  while(comment_q.length > 0){
-
-    const {cur, depth} = comment_q.pop()
-
-    if(cur.score > 999){
-      const thousands = Math.round(cur.score/1000)
-      const hundreds = Math.round(cur.score/100) % 10
-      if(hundreds==0){
-        cur.score = thousands + 'k'
-      }
-      else{
-        cur.score = thousands + '.' + hundreds + 'k'
-      }
-    }
-    const comment = Comment({
-      left_padding: initial_shift + shift_diff * depth,
-      post_link: 'https://reddit.com' + cur.permalink,
-      author_name: cur.author,
-      author_class: cur.is_submitter ? 'authorname bold': 'authorname',
-      comment_points: cur.score,
-      point_plural: cur.score == 1 ? '':'s',
-      time_ago: timeDifference(now, cur.created_utc*1000),
-      body: he.decode(cur.body_html),
-    })
-    out.push(comment)
-
-    if(out.length == 12)
-      div.innerHTML = out.join('\n')
-
-
-    //Add replies to queue
-    if(cur.replies){
-      replies = cur.replies.data.children
-      for(let i = replies.length-1; i>=0; i--){
-        if(replies[i].data.body_html)
-          comment_q.push({
-            cur: replies[i].data,
-            depth: depth + 1
-          })
-      }
-    }
-  }
-
-
-  div.innerHTML = out.join('\n')
 }
 
 function tryJson(str) {
@@ -150,35 +114,132 @@ function tryJson(str) {
     }
 }
 
+function loadJSON(url){
 
+}
+
+// Very useful helper function for renderDiv
 function timeDifference(current, previous) {
-
-    var elapsed = current - previous;
-
-    if (elapsed < msPerMinute) {
-         return Math.round(elapsed/1000) + ' seconds ago';
-    }
-
-    else if (elapsed < msPerHour) {
-         return Math.round(elapsed/msPerMinute) + ' minutes ago';
-    }
-
-    else if (elapsed < msPerDay ) {
-         return Math.floor(elapsed/msPerHour ) + ' hours ago';
-    }
-
-    else if (elapsed < msPerMonth) {
-        return Math.floor(elapsed/msPerDay) + ' days ago';
-    }
-
-    else if (elapsed < msPerYear) {
-        return Math.floor(elapsed/msPerMonth) + ' months ago';
-    }
-
-    else {
-        return Math.floor(elapsed/msPerYear ) + ' years ago';
+    const elapsed = current - previous;
+    const units = [msPerSecond, msPerMinute, msPerHour, msPerDay, msPerMonth, msPerYear, Number.MAX_VALUE]
+    const names = ['second',    'minute',    'hour',    'day',    'month',    'year',    'forever']
+    for(let i=1; i<units.length; i++){
+      if(elapsed < units[i]){
+        const val = Math.floor(elapsed/units[i-1])
+        if(val==1){
+          return val + ' ' + names[i-1] + ' ago'
+        }
+        else{
+          return val + ' ' + names[i-1] + 's ago'
+        }
+      }
     }
 }
+
+function formatScore(score){
+  // Converts score to string if it's large
+  if(score > 999){
+    const thousands = Math.round(score/1000)
+    const hundreds = Math.round(score/100) % 10
+    if(hundreds==0){
+      return thousands + 'k'
+    }
+    else{
+      return thousands + '.' + hundreds + 'k'
+    }
+  }
+  //IMPORTANT leave it as integer if we don't  format it
+  else
+    return score
+}
+
+
+function renderDiv(response, div, opts = defaults){
+  add_missing_defaults(opts)
+
+  const outputHTML = []
+  const post_data = response[0].data.children[0].data
+  const comments = response[1].data.children
+
+  if(opts.show_post_title)
+    outputHTML.push(Title({
+      title: post_data.title,
+      post_link: post_data.url
+    }))
+
+
+  // Renders post seperately since json structure
+  // is much different from a comment
+  if(opts.show_post){
+    console.log(post_data)
+
+  }
+
+  // List containing all the comment data to be rendered
+  const commentQ = []
+
+  if(opts.show_comments)
+    for(let i=comments.length-1; i>=0; i--){
+      //Ignores random blank comments with no html
+      if(comments[i].data.body_html)
+        commentQ.push({
+          item_data: comments[i].data,
+          depth: 0
+        })
+    }
+
+  const now = Date.now()
+  initial_shift = 8
+  shift_diff = 24
+  while(commentQ.length > 0){
+
+    const {item_data, depth} = commentQ.pop()
+    item_data.score = formatScore(item_data.score)
+
+    const comment = Comment({
+      left_padding: initial_shift + shift_diff * depth,
+      post_link: 'https://reddit.com' + item_data.permalink,
+      author_name: item_data.author,
+      author_class: item_data.is_submitter ? 'authorname bold': 'authorname',
+      comment_points: item_data.score,
+      point_plural: item_data.score == 1 ? '':'s',
+      time_ago: timeDifference(now, item_data.created_utc*1000),
+      body: he.decode(item_data.body_html),
+    })
+    outputHTML.push(comment)
+
+    //Add replies to queue
+    if(item_data.replies){
+      replies = item_data.replies.data.children
+      for(let i = replies.length-1; i>=0; i--){
+        if(replies[i].data.body_html)
+          commentQ.push({
+            item_data: replies[i].data,
+            depth: depth + 1
+          })
+      }
+    }
+  }
+
+  div.innerHTML = outputHTML.join('\n')
+
+  //Post processing
+  for(anchor of div.getElementsByTagName('a')){
+    if(opts.support_spoiler_links && spoiler_links.includes(anchor.getAttribute('href'))){
+      anchor.innerHTML += '<sup class="expand-button">+</sup>'
+      // Make the link literally do nothing, and add a helpful message for noobs
+      anchor.href = '#'
+      anchor.setAttribute('onclick', 'alert("This is a spoiler link, hover over it to see the spoiler text"); return false;')
+      anchor.setAttribute('title', "dw I replaced the spoiler text")
+    }
+    else{
+      if(opts.open_links_in_new_tab)
+        anchor.setAttribute('target', '_blank')
+    }
+  }
+}
+
+
 
   return{
     embed: embed
